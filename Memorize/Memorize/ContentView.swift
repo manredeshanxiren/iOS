97bdff7e130f56da1cfacbfaed5a12ea28a2097b
qingyangmi <m1448554339@gmail.com>
2025-03-19 13:33:19 +0800

    Lecture2
    feat：
    
    1.重构代码使用更多的函数和变量来封装
    
    2.使用ScollView 和 Group 来优化Card的显示效果
    
    fix：
    
    1.使用Group以及View的opacity属性修复了当isFaceUp设置为false的时候会导致卡片“被压扁的效果”
    
    2.同时采用了Button的disable属性解决了CardCount超过emojis数组索引范围时的crash问题

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -7,33 +7,77 @@
 
 import SwiftUI
 struct ContentView: View {
-    let emojis = ["👻", "😼", "🤡", "🐸"]
+    let emojis = ["👻", "😼", "🤡", "🐸", "🕷️", "👹", "🍭", "🙀","👻", "😼", "🤡", "🐸", "🕷️", "👹", "🍭", "🙀"]
+    
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            ScrollView {
+                cards
             }
+            Spacer()
+            cardCountAdjusters
         }
-        .foregroundColor(.blue)
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))], content: {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        })
+        .foregroundColor(.blue)
+    }
+                            
+                            
+    
+    var cardCountAdjusters: some View {
+        HStack{
+            Adder
+            Spacer()
+            Remover
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .padding()
+    }
+    
+    func cardCountAjdusters(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset >= emojis.count)
+    }
+    
+    var Adder: some View {
+        cardCountAjdusters(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    var Remover: some View {
+        cardCountAjdusters(by: -1, symbol: "rectangle.stack.badge.minus.fill")
     }
 }
 
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = true
     var body: some View {
         ZStack(alignment: .center) {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content)
                     .font(.largeTitle)
-            } else {
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
